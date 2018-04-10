b4099445cb80fc9789a420361cd34a1b8b309381
jamajosa <jhuijgens1@live.nl>
2018-04-10 11:00:22 +0200

    models aangepast

--- model/song.model.js
+++ model/song.model.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // song schema.
 const SongSchema = new Schema({
-    name: String,
-    length: String,
-    image: String
+    songid: String,
+    title: String,
+    artist: String,
+    album: String
 });
 
 module.exports = SongSchema;

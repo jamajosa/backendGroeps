a0ef170eec374ae5d31757ebcad5e83c305689ce
jamajosa <jhuijgens1@live.nl>
2018-04-10 11:25:59 +0200

    verandering van documentatie

--- model/user.model.js
+++ model/user.model.js
@@ -3,22 +3,27 @@ const Schema = mongoose.Schema;
 const Playlist = require('../model/playlist.model');
 
 const UserSchema = new Schema({
-  userName: String,
-  userPhjhhjhhjhhhjjhkhkhklaylists: [{
-    playlist: Playlist
-  }]
-}, {
-  timestamps: true
+    userName: String,
+    userPlaylists:
+    [{
+        playlist: Playlist
+    }]
+},
+{
+    timestamps: true
 });
 const User = mongoose.model('user', UserSchema);
 //create a playlist
-Playlist.find({
-  name: "Jordy"
-}, function(err, docs) {
-  if (docs.length) {} else {
-    const User = new User({
-      name: 'Jordy',
-    }).save();
-  }
+ Playlist.find({name : "Jordy"}, function (err, docs)
+{
+    if (docs.length)
+    {
+    }
+    else
+    {
+        const User = new User({
+            name: 'Jordy',
+        }).save();
+    }
 });
 module.exports = User;
